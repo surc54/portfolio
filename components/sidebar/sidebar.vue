c25e5708f84a5bdb7fdd18ffbdad1dfe8eb61cef
Adithya Haridas <adithya.haridas@infotechinc.com>
2021-04-25 13:27:27 -0400

    add footer

--- components/sidebar/sidebar.vue
+++ components/sidebar/sidebar.vue
@@ -1,16 +1,19 @@
 <template>
-  <div class="h-full py-4 bg-white">
-    <div class="mx-3">
+  <div class="flex flex-col flex-nowrap h-full pt-4 bg-white">
+    <div class="flex-1  mx-3">
       <sidebar-item class="my-1" icon="recent_actors">Resume</sidebar-item>
       <sidebar-item class="my-1" icon="assignment">Portfolio</sidebar-item>
     </div>
+
+    <sidebar-footer />
   </div>
 </template>
 
 <script>
+import SidebarFooter from './sidebar-footer.vue'
 import SidebarItem from './sidebar-item.vue'
 
 export default {
-  components: { SidebarItem }
+  components: { SidebarItem, SidebarFooter }
 }
 </script>
