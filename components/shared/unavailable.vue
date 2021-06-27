1bd462cd60f9ae1bebdee79815f7b2491bae51e9
Adithya Haridas <34602379+surc54@users.noreply.github.com>
2021-06-27 14:34:33 -0400

    Remove nuxt

--- components/shared/unavailable.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <div class="flex flex-col items-center justify-center text-center h-full">
-    <h1 class="text-lg">
-      This page is unavailable
-    </h1>
-    <p class="text-black text-opacity-60">Try again later</p>
-  </div>
-</template>
--- plugins/s-link.js
+++
@@ -1,4 +0,0 @@
-import Vue from 'vue'
-import SLink from '@/components/shared/s-link'
-
-Vue.component('s-link', SLink)
