adb1fa4d920016cec7d0db9d299f3378af95df19
Adithya Haridas <34602379+surc54@users.noreply.github.com>
2021-07-04 15:36:31 -0400

    Add gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  siteMetadata: {
+    title: "Portfolio",
+  },
+  plugins: ["gatsby-plugin-postcss", "gatsby-plugin-react-helmet"],
+};
---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import * as React from "react"
+
+const IndexPage = () => {
+  return (
+    <main>
+      <title>Home Page</title>
+      <h1>Website</h1>
+    </main>
+  )
+}
+
+export default IndexPage
